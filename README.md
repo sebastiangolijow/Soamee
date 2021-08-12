bf3bd0bf34d1e87e114f714e1aef66a637afa9c1
sebastiangolijow <you@example.com>
2021-08-12 12:45:51 +0200

    all

---
+++ README.md
@@ -0,0 +1,29 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+
+HTML : HTML represents the structure of the page, it tells the browser how to display the content, it provides the semantical description of the content. 
+
+CSS: CSS gives the style to the page, it describes how to render the page. Is a graphic design language to define and create the presentation of a structured document written in a markup language.
+
+JavaScript: It can be used on the client side to allow improvements in the UI and dynamic web pages. On the server side it is used to manage the information between the database and the site, it is done by an API.
+
+DOM: The name `DOM` comes from its acronym in English for 'Document Object Model'. When a browser loads a web page, it takes all the HTML content and generates a model for that content. Using Javascript code we can access and manipulate that model either by adding or removing elements, changing its attributes and also modifying its styles.
+
+WebGL: it is a new technologie to visualice 3D content in the network.
+
+
+portfolio: sebastiangolijow.github.io/
+github: https://github.com/sebastiangolijow?tab=repositories
+
+
---
+++ client/src/Redux/Reducer.js
@@ -0,0 +1,110 @@
+import { useEffect } from "react";
+
+const InitialState = {
+   Products: [],
+   Searchvalue: '',
+   Details: {},
+   Carrito: [],
+   Users:[],
+   timeLogged: []
+  };
+  
+function Reducer(state = InitialState, action) {
+
+    switch (action.type) { 
+      case "ALL_PRODUCTS":
+        return { ...state,
+        Products: action.payload 
+        };
+        case 'USER':
+          return {
+            ...state,
+            Users: action.payload
+          };
+        case 'LOGOUT':
+          return {
+            ...state,
+            Users: []
+          }
+          case 'CLEAR_ALL':
+            return {
+              Carrito: []
+            }
+        
+        case "SEARCHVALUE":
+          return { ...state,
+          Searchvalue: action.payload
+          };
+        case 'GET_DETAILS': 
+        return {
+          ...state,
+          Details: action.payload
+        };
+        case 'REMOVE': 
+        return {
+          ...state,
+          Carrito: state.Carrito.filter(item => item.sku !== action.payload)
+        }        
+        case 'addcart': {
+          return {
+            Carrito: state.Carrito.concat(action.payload)
+          }
+        }
+        case 'AGREGAR_CARRITO': 
+        return {
+          ...state,
+          Carrito: state.Carrito.concat(action.payload)
+        } 
+        case 'updateTime': 
+        let key = JSON.parse(window.localStorage.getItem('key'));
+        let email = key.email;
+        return {
+          ...state,
+          timeLogged: action.payload.filter(item => item.email === email)
+        } 
+      case "SEARCH":
+        if(!action.price && !action.date){
+          return {
+            ...state,
+            Products:action.payload
+          }
+        }
+        if(action.price && !action.date){
+          return {
+            ...state,
+            Products: action.payload.filter((item) => item.salePrice < action.price)
+          }
+        }
+        if(action.date && !action.price){
+          return {
+            ...state,
+            Products:  action.payload.filter((item) => parseInt(item.releaseDate.split('-')[0]) > parseInt(action.date) && parseInt(item.releaseDate.split('-')[0]) <  (parseInt(action.date) + 5) )
+          }
+        }
+      default:
+        return state;
+    }
+  }
+  
+  export default Reducer;
+
+
+    // }
+    
+        // if(action.date && !action.price){
+        //   return { 
+        //     ...state,
+        //     Products:
+        //   }
+        // }
+        // if(action.date && action.price){
+        //   return { 
+        //     ...state,
+        //     Temp: action.payload.filter((item) => item.salePrice < action.price),
+        //     Products: Temp.filter((item) => parseInt(item.releaseDate.split('-')[0]) > parseInt(action.date))
+        //   }
+        // }
+        //  else {
+        // return { ...state,
+        //   Products: action.payload
+        // }};
