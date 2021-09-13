499b1dc460df9c580a5125a39a08995eafec74c3
sebastiangolijow <you@example.com>
2021-09-14 00:15:57 +0200

    clean code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const soamee = require('./soamee')
 
 
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-//router.use('/api', tipos)
 router.use('/', soamee)
 
 
