6910a7dcc0b5f6cf3d62e26e66b12546ac61095d
sebastiangolijow <you@example.com>
2021-09-13 13:41:18 +0200

    'fixes'

---
+++ api/tests/models/author.spec.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+describe('Author model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => User.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        User.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when its a valid name', () => {
+        User.create({ name: 'Sebastian' });
+      });
+    });
+  });
+});
--- api/tests/routes/soamee.spec.js
+++ api/tests/routes/soamee.spec.js
@@ -4,21 +4,6 @@ var supertest = require('supertest-as-promised')(require('../../src/app'));
 var expect = require('chai').expect;
 const agent = session(app);
 
-
-// describe('Pokemon routes', () => {
-//   before(() => conn.authenticate()
-//   .catch((err) => {
-//     console.error('Unable to connect to the database:', err);
-//   }));
-//   beforeEach(() => Pokemon.sync({ force: true })
-//     .then(() => Pokemon.create(pokemon)));
-//   describe('GET /pokemons', () => {
-//     it('should get 200', () =>
-//       agent.get('/pokemons').expect(200)
-//     );
-//   });
-// });
-
 describe("/books", function () {
 
   it("GET responde con status 200 y listado de libros",(done) =>  {
