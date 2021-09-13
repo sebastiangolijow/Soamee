f78659e5768f857ef8a44fc9a6abb3f0774b12db
sebastiangolijow <you@example.com>
2021-09-13 12:34:57 +0200

    make it better

---
+++ client/src/Components/ModBook.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import {ModB} from '../Redux/Actions'
+
+const styleButton = {
+    borderRadius: "999px",
+    backgroundColor: "white",
+    margin: "20px",
+    outline: "none",
+    fontFamily: "fantasy",
+    height:'37px',
+    width:'85px',
+    fontSize:'11px'
+  };
+
+  const InputStyle = {
+    borderRadius: "20px",
+    border: "0",
+    width: "175px",
+    height: "30px",
+    padding: "7px",
+    outline: "none",
+    marginRight: "20px",
+  };
+
+
+export function ModBook({id}) {
+const dispatch = useDispatch()
+const [Input, setInput] = useState({ name: "", isbn:null, authorId:null, id:id});
+
+function handlechange1(e) {
+    setInput({ ...Input, [e.target.name]: e.target.value });
+}
+
+
+
+function  handlesubmit(e) {
+e.preventDefault();
+
+if(!Input.name){
+  return alert('Debes elgir un Nombre')
+}
+else if(!Input.isbn){
+    return alert('Debes elgir un ISBN')
+}
+console.log(Input)
+dispatch(ModB(Input));
+setInput({name:'', isbn:null, authorId:null});
+alert('Libro Actualizado');
+}
+    return (
+        <form>
+        <div>
+            <input
+                value={Input.name}
+                name="name"
+                onChange={handlechange1}
+                 style={InputStyle}
+                 placeholder='Nombre'
+            />
+            <input style={InputStyle} 
+                name='isbn' 
+                onChange={handlechange1} 
+                value={Input.isbn} 
+                placeholder='ISBN'
+            />
+            <input style={InputStyle} 
+                name='authorId' 
+                onChange={handlechange1} 
+                value={Input.authorId} 
+                placeholder='Autor ID'
+            />
+            <button style={styleButton} onClick={(e) => handlesubmit(e)}>Modificar Libro</button>
+
+        </div>
+        </form>
+    )
+}
